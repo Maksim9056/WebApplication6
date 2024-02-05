0d80bbc0119b659083868b1206af208e0fa43328
Maksim9056 <bobreczovm@inbox.ru>
2024-02-05 11:06:48 +0300

    Добавьте файлы проекта.

---
+++ WebApplication6/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using WebApplication6.Models;
+
+namespace WebApplication6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        // Другие свойства и методы по необходимости
+    }
+
+}
---
+++ WebApplication6/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication6.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
+
